5340d178cd58b7a55549882152a2935b421e0259
olinga-abdulaziz <abdulazizolinga17@gmail.com>
2022-01-27 11:28:59 +0300

    branch-b

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path=require('path')
 const bodyparser=require('body-parser')
 const dbconnection=require('./db')
 const routers=require('./routes/routes')
-const mpesa=require('./routes/mpesa')
+// const mpesa=require('./routes/mpesa')
 const memberRouter=require('./routes/memberRouter')
 const accountRouter=require('./routes/AccountRouter')
 const { engine }=require('express-handlebars')
@@ -41,7 +41,7 @@ app.use(bodyparser.json())
 app.use('/' , routers)
 app.use('/member',memberRouter)
 app.use('/account',accountRouter)
-app.use('/mpesa',mpesa)
+// app.use('/mpesa',mpesa)
 
 
 
